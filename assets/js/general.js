ed214f22732e39771c2726ac97c726f3f9965027
majesty25 <67669472+majesty25@users.noreply.github.com>
2022-02-16 03:08:54 +0000

    Update general.js

---
+++ assets/js/general.js
@@ -0,0 +1,12 @@
+function preload(e) {
+    e.preventDefault();
+    const container = document.querySelector("#CONTAINER");
+    const img = document.querySelector("#image-btn");
+    container.style.background = "whilte";
+    img.style.background = "white";
+
+}
+
+window.addEventListener("load", (e) => {
+    
+})
