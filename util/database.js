685f0930d5f9abca70291d9ab98256cf0db6d193
His Majesty <nyankson25@gmail.com>
2022-09-10 21:35:43 +0000

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-// let url ="mongodb+srv://STEPHENNYANKSON:tvvq8KSYSuN4vWXi@cluster0.j5vgn.mongodb.net/nyankie?retryWrites=true&w=majority";
-let url = "mongodb://127.0.0.1:27017/nyankie";
+let url ="mongodb+srv://STEPHENNYANKSON:tvvq8KSYSuN4vWXi@cluster0.j5vgn.mongodb.net/nyankie?retryWrites=true&w=majority";
+// let url = "mongodb://127.0.0.1:27017/nyankie";
 const connection = mongoose.createConnection(
   url
 );
